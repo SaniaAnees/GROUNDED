8e90b41135ba259ee98c53e0ef470a557bc6cd74
SaniaAnees <saniaanees91@gmail.com>
2026-08-01 19:03:30 +0530

    feat: implement vector retrieval function
    - Use manual cosine similarity calculation for embeddings
    - Support top-k result scoring

---
+++ retrieve.py
@@ -0,0 +1,26 @@
+import numpy as np
+from ingest import ingest
+from embed import embed
+from sentence_transformers import SentenceTransformer
+def retrieve(embedded_chunk,question):
+    model=SentenceTransformer("all-MiniLM-L6-v2")
+    emb_que=model.encode(question)
+    for file in embedded_chunk:
+        file["similarity_score"]=np.dot(file['embeddings'], emb_que) / (np.linalg.norm(file['embeddings']) * np.linalg.norm(emb_que))
+    return sorted(embedded_chunk,key=lambda x: x['similarity_score'],reverse=True)[:5]
+        
+
+
+def main():
+    q=input("enter your question?")
+    folder_path=input("enter your file path")
+    folder=ingest(folder_path)
+    echunk=embed(folder)
+    schunk=retrieve(echunk,q)
+    print(f"Top 5 results for: {q}")
+    for result in schunk:
+        print(f"File: {result['filename']}, Similarity: {result['similarity_score']}")
+    return schunk
+    
+if __name__=="__main__":
+    main()
